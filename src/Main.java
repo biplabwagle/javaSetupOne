67168ec537099d53cd77792076b84294a18decdf
biplab-w <waglebip@gmail.com>
2023-11-15 21:48:42 -0600

    Added print message

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
             System.out.println("This is a test file");
-
+            System.out.println("This is a second test statement");
     }
 }
