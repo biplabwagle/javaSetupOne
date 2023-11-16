4e085d2d713787aa1e93c4bf0ecf5296a7f9879f
biplab-w <waglebip@gmail.com>
2023-11-15 21:53:07 -0600

    Added package com.a, class A

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import com.a.A;
+
 public class Main {
     public static void main(String[] args) {
             System.out.println("This is a test file");
             System.out.println("This is a second test statement");
+            A ab = new A();
+            ab.printMessage();
     }
 }
---
+++ src/com/a/A.java
@@ -0,0 +1,9 @@
+package com.a;
+
+public class A {
+    String message ="This statement is from class A";
+
+    public void printMessage(){
+        System.out.println(message);
+    }
+}
